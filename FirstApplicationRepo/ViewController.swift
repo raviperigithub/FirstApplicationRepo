c53a14052e6934fc7a50ddf06aa2aca7475873e8
Harsha Ravi <speri@ncsu.edu>
2019-07-01 22:16:53 -0700

    Initial project setup

---
+++ FirstApplicationRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApplicationRepo
+//
+//  Created by Srinivasa Ravi Teja Peri on 7/1/19.
+//  Copyright © 2019 HarshaRaviGitHub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
